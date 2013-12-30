08866852826db3f378ac84b0116d572a12aa7f9d
zhongtenghui <364755805@qq.com>
2013-12-30 13:42:09 +0800

    修改install.sh，使其对已存在正确链接的.vimrc时，不进行链接，修改echo信息位置

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # 克隆vundle版本库
-echo "克隆vundle"
 if ! [ -e ~/.vim/bundle/vundle/.git ]; then
 	if [ -e ~/.vim/bundle/vundle ]; then
 		rm -rf ~/.vim/bundle/vundle
 	fi
 
+	echo "克隆vundle"
 	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle 
 fi
 
-echo "创建.vimrc"
-if ! [ -L ~/.vimrc ]; then
+if  ! [ -L ~/.vimrc ] || ! [ ~/.vimrc -ef ~/.vim/.vimrc ] ; then
+	echo "创建.vimrc"
 	rm -rf ~/.vimrc
 	ln -s ~/.vim/.vimrc ~/.vimrc
 fi
@@ -19,6 +19,7 @@ fi
 GenerateClangComplete=~/.vim/bin/generate_clang_complete.sh
 if [ -f $GenerateClangComplete ]; then
 	if ! [ -x $GenerateClangComplete ]; then
+		echo "为generate_clang_complete.sh添加执行权限"
 		chmod +x $GenerateClangComplete 
 	fi
 fi
