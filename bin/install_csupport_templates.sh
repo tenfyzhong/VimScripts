5e8f813b6886406e61a59e9a457f4fe1e2c1837e
zhongtenghui <364755805@qq.com>
2014-01-24 09:48:28 +0800

    修改c.vim的Templates链接方式

--- bin/install_csupport_templates.sh
+++ bin/install_csupport_templates.sh
@@ -4,12 +4,15 @@
 TEMPLATES=~/.vim/templates/csupport/Templates
 MYSELFTEMPLATES=~/.vim/templates/csupport/MyselfTemplates
 DEFAULTTEMPLATES=~/.vim/bundle/c.vim/c-support/templates/Templates 
-if ! [ -L ~/.vim/templates/csupport/Templates ]; then
-	echo "修改csupport模板"
-	if [ -f $MYSELFTEMPLATES ]; then
+if [ -f $DEFAULTTEMPLATES ]; then
+	if ! [ -L $TEMPLATES ] || ! [ $MYSELFTEMPLATES -ef $MYSELFTEMPLATES ]; then
+		rm -f $TEMPLATES
+		echo "修改csupport模板"
+		if ! [ -f $MYSELFTEMPLATES ]; then
+			cp $DEFAULTTEMPLATES $MYSELFTEMPLATES
+		fi
+
 		ln -s $MYSELFTEMPLATES $TEMPLATES
-	elif [ -f $DEFAULTTEMPLATES ]; then
-		ln -s $DEFAULTTEMPLATES $TEMPLATES
 	fi
 fi
 
