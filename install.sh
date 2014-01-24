b6bb4a815e969a56f0541dd270512b1d65344a51
zhongtenghui <364755805@qq.com>
2014-01-24 10:10:11 +0800

    使用数组重构install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 
-SCRIPTS=''
+SCRIPTS=(\
+	~/.vim/bin/install_clone_vundle.sh \
+	~/.vim/bin/install_vimrc.sh \
+	~/.vim/bin/install_generate_clang_complete.sh \
+	~/.vim/bin/install_load_plugin.sh \
+	~/.vim/bin/install_csupport_templates.sh \
+	~/.vim/bin/install_ywvim_codetable.sh)
 
-# 克隆vundle版本库
-SCRIPTS=~/.vim/bin/install_clone_vundle.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
-
-# 链接.vimrc
-SCRIPTS=~/.vim/bin/install_vimrc.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
-
-# 给generate_clang_complete.sh加执行权限
-SCRIPTS=~/.vim/bin/install_generate_clang_complete.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
-
-# 运行BundleInstall加载插件
-SCRIPTS=~/.vim/bin/install_load_plugin.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
-
-# 加载模板
-SCRIPTS=~/.vim/bin/install_csupport_templates.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
-
-# 安装ywvim码表
-SCRIPTS=~/.vim/bin/install_ywvim_codetable.sh
-[[ -f $SCRIPTS ]] && sh $SCRIPTS
+for script in ${SCRIPTS[@]}; do
+	[[ -f $script ]] && sh $script
+done
