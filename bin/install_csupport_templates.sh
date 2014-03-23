6f4ac5e14ad01ca980a70c14426168c8d88cfdd8
zhongtenghui <364755805@qq.com>
2014-03-23 12:08:14 +0800

    加入BaseTemplates记录不同的模板

--- bin/install_csupport_templates.sh
+++ bin/install_csupport_templates.sh
@@ -3,13 +3,21 @@
 # 修改csupport模板
 TEMPLATES=~/.vim/templates/csupport/Templates
 MYSELFTEMPLATES=~/.vim/templates/csupport/MyselfTemplates
+BASETEMPLATES=~/.vim/templates/csupport/BaseTemplates
 DEFAULTTEMPLATES=~/.vim/bundle/c.vim/c-support/templates/Templates 
-if [ -f $DEFAULTTEMPLATES ]; then
-	if ! [ -L $TEMPLATES ] || ! [ $MYSELFTEMPLATES -ef $MYSELFTEMPLATES ]; then
+
+COPYTEMPLATES=$BASETEMPLATES
+
+if ! [ -f $COPYTEMPLATES ]; then
+	COPYTEMPLATES=$DEFAULTTEMPLATES
+fi
+
+if [ -f $COPYTEMPLATES ]; then
+	if ! [ -L $TEMPLATES ] || ! [ $TEMPLATES -ef $MYSELFTEMPLATES ]; then
 		rm -f $TEMPLATES
 		echo "修改csupport模板"
 		if ! [ -f $MYSELFTEMPLATES ]; then
-			cp $DEFAULTTEMPLATES $MYSELFTEMPLATES
+			cp $COPYTEMPLATES $MYSELFTEMPLATES
 		fi
 
 		ln -s $MYSELFTEMPLATES $TEMPLATES
