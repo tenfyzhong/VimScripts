86c2a240a342d6324073b3ded1e607a668028777
tenfy <364755805@qq.com>
2014-06-23 11:15:41 +0800

    将vimshell的安装脚本加入install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ SCRIPTS=(\
 	~/.vim/bin/install_generate_clang_complete.sh \
 	~/.vim/bin/install_load_plugin.sh \
 	~/.vim/bin/install_csupport_templates.sh \
-	~/.vim/bin/install_ywvim_codetable.sh)
+    ~/.vim/bin/install_ywvim_codetable.sh \
+    ~/.vim/bin/install_vimshell.sh )
 
 for script in ${SCRIPTS[@]}; do
 	[[ -f $script ]] && sh $script
