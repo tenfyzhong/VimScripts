04bad102272da37a02cb72ab99178365ffcf33b3
tenfy <364755805@qq.com>
2014-07-03 20:35:05 +0800

    修改csupport的模板实现

--- bin/install_csupport_templates.sh
+++ bin/install_csupport_templates.sh
@@ -1,26 +1,9 @@
 #!/bin/bash 
 
 # 修改csupport模板
-TEMPLATES=~/.vim/resource/csupport/Templates
 MYSELFTEMPLATES=~/.vim/resource/csupport/MyselfTemplates
 BASETEMPLATES=~/.vim/resource/csupport/BaseTemplates
-DEFAULTTEMPLATES=~/.vim/bundle/c.vim/c-support/templates/Templates 
 
-COPYTEMPLATES=$BASETEMPLATES
-
-if ! [ -f $COPYTEMPLATES ]; then
-	COPYTEMPLATES=$DEFAULTTEMPLATES
+if ! [ -f $MYSELFTEMPLATES ]; then
+    cp $BASETEMPLATES $MYSELFTEMPLATES
 fi
-
-if [ -f $COPYTEMPLATES ]; then
-	if ! [ -L $TEMPLATES ] || ! [ $TEMPLATES -ef $MYSELFTEMPLATES ]; then
-		rm -f $TEMPLATES
-		echo "修改csupport模板"
-		if ! [ -f $MYSELFTEMPLATES ]; then
-			cp $COPYTEMPLATES $MYSELFTEMPLATES
-		fi
-
-		ln -s $MYSELFTEMPLATES $TEMPLATES
-	fi
-fi
-
