e2d25290d30d1f90c236a8040bfb1c3fa9ae6861
tenfy <364755805@qq.com>
2014-07-03 20:59:29 +0800

    不输出警告信息

--- bin/install_vimshell.sh
+++ bin/install_vimshell.sh
@@ -4,8 +4,8 @@ cd ~/.vim/bundle/vimproc.vim
 declare -u os
 os=`uname -o`
 if [ "$os" == "CYGWIN" ]; then
-    make -f make_cygwin.mak
+    make -f make_cygwin.mak > /dev/null 2>&1
 else
-    make
+    make > /dev/null 2>&1
 fi
 
