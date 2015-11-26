4719060e8ecb9d511b14fbd540903a6d307efd96
tenfy <364755805@qq.com>
2015-11-26 22:26:01 +0800

    修正vundle的更新插件

--- bin/install_clone_vundle.sh
+++ bin/install_clone_vundle.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 # 克隆vundle版本库
-VUNDLE=~/.vim/bundle/vundle
-if ! [ -e $VUNDLE/.git ]; then
-	if [ -e $VUNDLE ]; then
-		rm -rf $VUNDLE
-	fi
-
-	echo "克隆vundle"
-	git clone https://github.com/gmarik/vundle.git $VUNDLE
-
-# else
-# 	cd $VUNDLE
-# 	echo "拉回vundle"
-# 	git pull
-# 	cd -
-fi
-
+git submodule update --init --recursive
