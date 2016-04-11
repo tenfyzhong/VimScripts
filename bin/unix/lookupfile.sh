71b66513351244e393493a33830187cacf8de2cc
tenfy <364755805@qq.com>
2016-04-11 15:27:59 +0800

    忽略lib和so下的文件

--- bin/unix/lookupfile.sh
+++ bin/unix/lookupfile.sh
@@ -31,7 +31,8 @@ find $project -not -regex '.*\.\(png\|gif\|mp3\|mp4\|pdf\|o\|d\|a\|so\|class\|ob
     -a -not -regex '\.git.*' -a -not -regex '\.hg.*' -a -not -name 'NO_RESOURCES' \
     -a -not -name "*~" -a -not -name 'cscope.*' -a -not -name ".npmignore" \
     -a -not -name ".NERDTreeBookmarks" -a -not -name ".dblite" -a -not -name ".ycm_extra_conf.py"\
-    -a -not -wholename '*.svn*' -a -not -wholename "*.git*" \
+    -a -not -wholename '*.svn*' -a -not -wholename "*.git*" -a -not -wholename "*/lib/*" \
+    -a -not -wholename "*/so/*" \
     -a -not -wholename "*/.undofile/*" -a -not -wholename ".hg/*" \
     -type f -printf "%f\t%p\t1\n" | sort -f >> $project/.lookupfile_tags
 
