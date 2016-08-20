2e687ff400fac52b6da85c29c17c7fee52dd0f4b
tenfy <364755805@qq.com>
2016-08-20 10:04:10 +0800

    delete

--- bin/unix/install_bash_support.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-TEMPLATESRC=~/.vim/bundle/bash-support.vim/bash-support/templates/Templates
-MYSELFTEMPLATES=~/.vim/resource/bash-support/MyselfTemplates
-if ! [ -f $MYSELFTEMPLATES ]; then
-    cp $TEMPLATESRC $MYSELFTEMPLATES
-fi
--- bin/unix/install_csupport_templates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# 修改csupport模板
-MYSELFTEMPLATES=~/.vim/resource/csupport/MyselfTemplates
-BASETEMPLATES=~/.vim/resource/csupport/BaseTemplates
-
-if ! [ -f $MYSELFTEMPLATES ]; then
-    cp $BASETEMPLATES $MYSELFTEMPLATES
-fi
