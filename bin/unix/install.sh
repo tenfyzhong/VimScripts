2bb36a784b20f5d34625cd1a21dc773913e31881
tenfy <364755805@qq.com>
2016-09-21 19:23:25 +0800

    删除没用的文件

--- bin/unix/install.sh
+++ bin/unix/install.sh
@@ -9,12 +9,7 @@ cd $UNIX_INSTALL_PATH/../..
 SCRIPTS=(\
 	$UNIX_INSTALL_PATH/install_clone_vundle.sh \
 	$UNIX_INSTALL_PATH/install_vimrc.sh \
-	$UNIX_INSTALL_PATH/install_generate_clang_complete.sh \
-	$UNIX_INSTALL_PATH/install_load_plugin.sh \
-	$UNIX_INSTALL_PATH/install_csupport_templates.sh \
-    $UNIX_INSTALL_PATH/install_ywvim_codetable.sh \
-    $UNIX_INSTALL_PATH/install_vimshell.sh \
-    $UNIX_INSTALL_PATH/install_bash_support.sh )
+    $UNIX_INSTALL_PATH/install_load_plugin.sh)
 
 for script in ${SCRIPTS[@]}; do
 	[[ -f $script ]] && sh $script
