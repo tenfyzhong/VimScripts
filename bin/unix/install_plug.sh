74178b76fd65c93f1c63634e2281a9330e1cdf9f
tenfy <364755805@qq.com>
2017-02-25 21:47:50 +0800

    创建plug.vim的软链

--- bin/unix/install_plug.sh
+++ bin/unix/install_plug.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/junegunn/vim-plug.git ~/.vim/bundle/vim-plug
+git clone https://github.com/junegunn/vim-plug.git ~/.vim/bundle/vim-plug 2> /dev/null
+ln -s ~/.vim/bundle/vim-plug/plug.vim ~/.vim/autoload/plug.vim 2> /dev/null
