1bb2c26729ca35a81b6d7f05f99a2a0dfdacf858
tenfy <tenfy@tenfy.cn>
2023-01-16 10:06:08 +0800

    lspkind.nvim

--- nvim_post/nvim-cmp.lua
+++ nvim_post/nvim-cmp.lua
@@ -31,6 +31,8 @@ local super_prev = cmp.mapping(function()
     end
 end, { "i", "s" })
 
+local lspkind = require('lspkind')
+
 cmp.setup({
     enabled = function()
         -- disable completion in comments
@@ -79,7 +81,21 @@ cmp.setup({
         -- { name = 'snippy' }, -- For snippy users.
     }, {
         { name = 'buffer' },
-    })
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+                -- ...
+                return vim_item
+            end
+        })
+    }
 })
 
 -- Set configuration for specific filetype.
---
+++ vimrcs/lspkind.nvim.vim
@@ -0,0 +1,12 @@
+"==============================================================
+"    file: lspkind.nvim.vim
+"   brief: 
+" VIM Version: 9.0
+"  author: tenfyzhong
+"   email: tenfy@tenfy.cn
+" created: 2023-01-16 09:52:50
+"==============================================================
+if !has('nvim')
+  finish
+endif
+Plug 'onsails/lspkind.nvim'
